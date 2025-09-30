a6316ec096c98687371d537b570871a4e378429f
Tung Le <leesontungggg@gmail.com>
2025-09-30 21:44:40 +0700

    fix code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 export default function Footer() {
@@ -7,14 +9,31 @@ export default function Footer() {
     <footer className="w-full bg-[#3669B5] text-white py-12 px-4 font-manrope">
       <div className="container mx-auto flex flex-col md:flex-row items-start">
         <div className="mb-8 md:mb-0">
-          <img src="/logo.png" alt="Logo" className="h-[240px] w-auto" />
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              height={240}
+              width={240}
+              className="w-auto"
+              priority
+            />
+          </Link>
+
+          {/* <img src="/logo.png" alt="Logo" className="h-[240px] w-auto" /> */}
         </div>
 
         <div className="flex flex-col ml-auto leading-16 text-[32px] font-manrope">
           CÔNG TY TNHH MTV MINH PHI <br />
           263 Trần Hưng Đạo, Khu phố Tây A, Phường Đông Hoà, Tp.HCM <br />
-          minhphiprinting@outlook.com <br />
-          0908 846 050 - 0909 444 030
+          <div>
+            <a href="mailto:minhphiprinting@outlook.com">
+              minhphiprinting@outlook.com
+            </a>{" "}
+            <br />
+            <a href="tel:0908846050">0908 846 050</a> -{" "}
+            <a href="tel:0909444030">0909 444 030</a>
+          </div>
         </div>
       </div>
     </footer>
